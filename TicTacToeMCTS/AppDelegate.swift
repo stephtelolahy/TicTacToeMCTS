256253b25e52e00e0643695eb4d30c8c139af890
Stéphano TELOLAHY <hugues.stephano.telolahy@devoteam.com>
2021-05-30 07:10:23 +0200

    initial commit

---
+++ TicTacToeMCTS/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  TicTacToeMCTS
+//
+//  Created by Hugues Stéphano TELOLAHY on 30/05/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ TicTacToeMCTS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToeMCTS
+//
+//  Created by Hugues Stéphano TELOLAHY on 30/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
